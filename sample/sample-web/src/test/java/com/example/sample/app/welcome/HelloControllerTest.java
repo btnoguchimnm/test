7e33a7eae07b140332a8635f6e18959377c7f87b
btnoguchimnm <Minami.Noguchi.bp@nttdata.com>
2020-09-03 17:54:45 +0900

    add HelloControllerTest

---
+++ sample/sample-web/src/test/java/com/example/sample/app/welcome/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.sample.app.welcome;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.spy;
+
+import java.util.Locale;
+
+import org.junit.Test;
+import org.springframework.ui.Model;
+
+public class HelloControllerTest {
+
+    @Test
+    public void testHome() {
+        HelloController controller = new HelloController();
+        Model model = spy(Model.class);
+        String result = controller.home(Locale.JAPAN, model);
+        assertThat(result, is("welcome/home"));
+    }
+
+}
